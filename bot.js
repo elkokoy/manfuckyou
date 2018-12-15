0fcfa0a9fd2f5b70a9cac7768ccaa0ec6515b4c1
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-15 22:34:50 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Eris = require("eris");
+var x5bzteam = new Eris("MzUxMzY2NTA0MDY4OTM5Nzc3.DvbfQw.FyrawIl2Q0T7jNfCClilNMOBubY");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "518933084792684544";
+nick = "welcomeguys";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
